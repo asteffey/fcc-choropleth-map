e5558126fd02f4871bbd8b0193d4aa60396217da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-16 06:03:36 +0000

    Deploy asteffey/fcc-choropleth-map to github.com/asteffey/fcc-choropleth-map.git:gh-pages

---
+++ precache-manifest.2eb2b46f7e3fdbd1987c8151e584c307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea3a6b88937e1fac496fafe2a7944d5",
+    "url": "/fcc-choropleth-map/index.html"
+  },
+  {
+    "revision": "77721c652a926a107536",
+    "url": "/fcc-choropleth-map/static/css/main.63d6bcf0.chunk.css"
+  },
+  {
+    "revision": "2207ed46c80e209c569e",
+    "url": "/fcc-choropleth-map/static/js/2.109ca9cb.chunk.js"
+  },
+  {
+    "revision": "3f06f0f9ff8e6a78c5332d3eed7e4fd4",
+    "url": "/fcc-choropleth-map/static/js/2.109ca9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77721c652a926a107536",
+    "url": "/fcc-choropleth-map/static/js/main.790099f3.chunk.js"
+  },
+  {
+    "revision": "7c3df898e3cb73ec3c2a",
+    "url": "/fcc-choropleth-map/static/js/runtime-main.c4b821fa.js"
+  }
+]);
